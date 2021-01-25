bbef4554897a15d326395e66ba0faa39af6114da
Lav Kumar Pandey <lav7000@gmail.com>
2021-01-25 11:26:21 -0500

    Initial commit

---
+++ src/main/java/com/gigatorb/ems/EmsSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gigatorb.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsSpringBootBackendApplication.class, args);
+	}
+
+}
