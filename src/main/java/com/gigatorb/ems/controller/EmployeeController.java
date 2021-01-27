7ff2602e0ee7facbdd5e3a4b5bd989815e19d8e1
Lav Kumar Pandey <lav7000@gmail.com>
2021-01-27 18:54:03 -0500

    Rest Controller - CRUD APIs

--- src/main/java/com/gigatorb/ems/controller/EmployeeController.java
+++ src/main/java/com/gigatorb/ems/controller/EmployeeController.java
@@ -3,12 +3,12 @@ package com.gigatorb.ems.controller;
 import com.gigatorb.ems.model.Employee;
 import com.gigatorb.ems.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -25,5 +25,31 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+        Employee employee = employeeService.updateEmployee(id, employeeDetails);
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeService.getEmployeeById(id);
+        employeeService.deleteEmployee(employee);
+        Map<String, Boolean> response = new HashMap<String, Boolean>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/gigatorb/ems/service/EmployeeService.java
+++ src/main/java/com/gigatorb/ems/service/EmployeeService.java
@@ -1,9 +1,15 @@
 package com.gigatorb.ems.service;
 
+import com.gigatorb.ems.exception.ResourceNotFoundException;
 import com.gigatorb.ems.model.Employee;
 import com.gigatorb.ems.repository.EmployeeJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -14,5 +20,30 @@ public class EmployeeService {
 
     public List<Employee> getAllEmployees(){
         return employeeJpaRepository.findAll();
+    }
+
+    public Employee createEmployee(Employee employee){
+        return employeeJpaRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id){
+        Employee employee  = employeeJpaRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Employee not exists with id "+id));
+        return employee;
+    }
+
+    public Employee updateEmployee(Long id, Employee employeeDetails){
+        Employee employee  = employeeJpaRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Employee not exists with id "+id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        return employeeJpaRepository.save(employee);
+    }
+
+    public void deleteEmployee(Employee employee){
+        employeeJpaRepository.delete(employee);
     }
 }
