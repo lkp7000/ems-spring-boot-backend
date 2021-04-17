116cdd8c4c99f2d68bb505366fc0ccb166f8e7d0
Lav Kumar Pandey <lav7000@gmail.com>
2021-04-17 16:10:30 +0530

    Rest Controller - Model Update

--- src/main/java/com/gigatorb/ems/model/Employee.java
+++ src/main/java/com/gigatorb/ems/model/Employee.java
@@ -1,6 +1,9 @@
 package com.gigatorb.ems.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -9,7 +12,8 @@ import javax.persistence.*;
 @Entity
 @Table( name = "employees")
 public class Employee {
-    @Getter @Setter
+    @Getter
+    @Setter
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
